059ad5596be7547ff5cfb708b343d10889e212b0
Yevgeniy G <gersen.e.a@gmail.com>
2024-02-01 09:24:38 +0600

    Добавил RooController

---
+++ Hotel.ATR.Portal/Controllers/RoomController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.ATR.Portal.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoomList()
+        {
+            return View();
+        }
+    }
+}
