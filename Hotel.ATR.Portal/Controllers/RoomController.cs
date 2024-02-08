da9140d80c39451183b81cc90e263804bd9f50ba
Yevgeniy G <gersen.e.a@gmail.com>
2024-02-08 10:43:55 +0600

    Поработали с контролерами

--- Hotel.ATR.Portal/Controllers/RoomController.cs
+++ Hotel.ATR.Portal/Controllers/RoomController.cs
@@ -1,18 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel.ATR.Portal.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Hotel.ATR.Portal.Controllers
 {
     public class RoomController : Controller
     {
-        public IActionResult Index()
+        private IWebHostEnvironment hostEnv;
+        public RoomController(IWebHostEnvironment hostEnv)
         {
-            return View();
+            this.hostEnv = hostEnv;
         }
 
+        public IActionResult Index(string email)
+        {
+            ViewBag.CurrentTime = DateTime.Now.ToString();
+            TempData["CurrentTimeTD"] = DateTime.Now.ToString();
+            return View((object)email);
+        }
+
+        public ContentResult IndexText()
+        {
+            string message = "some text";
+            return Content(message, "text/plain", Encoding.Default);
+        }
+         
+        //public JsonResult IndexBeep()
+        //{
+        //    string message = "some text";
+        //    return Json(message, JsonRequestBehavior.AllowGet);
+        //}
+
+        public FileResult GetFile()
+        {
+            string path = Path.Combine(hostEnv.WebRootPath, "style.css");
+
+            return File(path, "text/plain", "FileName.css");
+        }
         public IActionResult RoomList()
         {
             return View();
@@ -21,6 +52,23 @@ namespace Hotel.ATR.Portal.Controllers
         public IActionResult RoomDetails()
         {
             return View();
+            //return View("Index");
+            //return RedirectToAction("RoomList");
+        }
+
+        [HttpPost]
+        public async Task< IActionResult> CreateSubscribe(IFormFile photo, User  user)//string email
+        {
+            string path = Path.Combine(hostEnv.WebRootPath, photo.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+               await photo.CopyToAsync(stream);
+            }
+
+            //return View("~/Views/Home/Privacy.cshtml");
+            ///TODO
+            return View("Index", new { email= user.email } );
+
         }
     }
 }
